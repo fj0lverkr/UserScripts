151ba8282598624b44f400652d8d8fbb642746bd
nils <nils.nahooy@live.com>
2022-02-17 13:32:21 +0100

    MS changed classname of slogan container. Updated script accordingly.

--- Outlook.com hide banner ad.user.js
+++ Outlook.com hide banner ad.user.js
@@ -13,23 +13,24 @@
     introduced in GM/TM 1.0.   It restores the sandbox.
 */
 
-waitForKeyElements ("._1fti_QgAzqGWPGlqh_FSvI, ._28ithXDZzMqSN0YAG2rCVn", hideElem)
-waitForKeyElements ("._6EkCIBulssv0eDQ55G3yH", setSillySlogan)
+waitForKeyElements("._1fti_QgAzqGWPGlqh_FSvI, ._28ithXDZzMqSN0YAG2rCVn", hideElem)
+//waitForKeyElements ("._6EkCIBulssv0eDQ55G3yH", setSillySlogan)
+waitForKeyElements("._2tvCNqil81Lsi6owqrTDMq", setSillySlogan)
 
-function hideElem(node){
-    node.hide()
+function hideElem(node) {
+  node.hide()
 }
 
-function setSillySlogan(node){
-     let words = ['At least it\'s not Gmail',
-             'kooltuO',
-             'ʞoolʇnO',
-             'Powered By Satan',
-             'Vooruitzicht',
-             'Premium included in next vaccine',
-             'Lorem ipsum...'
-            ]
-     let seed = Math.floor(Math.random() * words.length)
-     let word = words[seed]
-     node.html(word)
+function setSillySlogan(node) {
+  let words = ['At least it\'s not Gmail',
+    'kooltuO',
+    'ʞoolʇnO',
+    'Powered By Satan',
+    'Vooruitzicht',
+    'Premium included in next vaccine',
+    'Lorem ipsum...'
+  ]
+  let seed = Math.floor(Math.random() * words.length)
+  let word = words[seed]
+  node.html(word)
 }
